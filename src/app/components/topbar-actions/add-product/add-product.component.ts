9b4dbeb912f19db7206b2410cba61fad8130024c
rahmanhamim <rahmanhamiminfo@gmail.com>
2025-05-15 14:49:13 +0600

    product add form added

--- src/app/components/topbar-actions/add-product/add-product.component.ts
+++ src/app/components/topbar-actions/add-product/add-product.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzInputGroupComponent, NzInputModule } from 'ng-zorro-antd/input';
 import { NzModalModule } from 'ng-zorro-antd/modal';
 
 @Component({
   selector: 'app-add-product',
   standalone: true,
-  imports: [NzButtonModule, NzModalModule],
+  imports: [NzButtonModule, NzModalModule, ReactiveFormsModule, NzInputModule],
   templateUrl: './add-product.component.html',
   styleUrl: './add-product.component.css',
 })
@@ -27,5 +34,20 @@ export class AddProductComponent {
 
   handleCancel(): void {
     this.isVisible = false;
+  }
+
+  form = new FormGroup({
+    title: new FormControl('', { validators: [Validators.required] }),
+    price: new FormControl('', {
+      validators: [Validators.required, Validators.min(0)],
+    }),
+    description: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    thumbnail: new FormControl('', { validators: [Validators.required] }),
+  });
+
+  onSubmit() {
+    console.log(this.form.value);
   }
 }
